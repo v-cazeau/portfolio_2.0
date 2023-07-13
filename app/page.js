c3b638e9829a24e1bdef23cd6c559e0bffdee50a
Véronie R. Cazeau <veronie.cazeau@gmail.com>
2023-07-13 01:35:45 -0400

    UPDATED layout; WIP

--- app/page.js
+++ app/page.js
@@ -1,11 +1,17 @@
+import Certifications from '@/components/Certifications'
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
 import Hero from '@/components/Hero'
 import Projects from '@/components/Projects'
 
 export default function Home() {
   return (
     <main>
+      <Header />
       <Hero />
       <Projects />
+      <Certifications />
+      <Footer />
     </main>
   )
 }
